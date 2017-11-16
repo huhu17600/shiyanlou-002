2ddaae9c02fd76fb97f8caaeaf4ef79e02de5e9f
676574039 <676574039@qq.com>
2017-11-16 11:38:38 +0800

    a new file

--- calculator.py
+++ calculator.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python3
 
+class Config(object):
+	def __init__(self):
+		self._config = {}
+	def set_config(self,value):
+		Str = value.split(':')
+		self._config[Str[0].strip()] = Str[1].strip()
+	def get_config(self,value):
+		return self._config[value]
+class UserData(object):
+	def __init__(self):
+		self._userdata = {}
+	def set_userdata(self,value):
+		Str = value.split(',')
+		self._userdata[Str[0].strip()] = Str[1].strip()
+	def get_userdata(self,value):
+		return self._userdata[value]
+	def calculate(self):
+		for key,value in self._userdata:
+			income = value
+			wages = value
+			wages = wages - wages * 0.165 - 3500
+			if(wages > 80000):
+				wages = wages * 0.45 - 13505
+			elif(wages > 55000):
+				wages = wages * 0.35 - 5505
+			elif(wages > 35000):
+				wages = wages * 0.3 - 2755
+			elif(wages > 9000):
+				wages = wages * 0.25 - 1005
+			elif(wages > 4500):
+				wages = wages * 0.2 - 555
+			elif(wages > 1500):
+				wages = wages * 0.1 - 105
+			elif(wages > 0 ):
+				wages = wages * 0.03
+			else:
+				wages = 0
+			income = income - wages - income * 0.165
+			print("{}:{:.2f}".format(key,income))
+
+if __name__ = '__main__':
 import sys
 
-def calculate(wages):
-	income = wages
-	wages = wages - wages * 0.165 - 3500
-	if(wages > 80000):
-		wages = wages * 0.45 - 13505
-	elif(wages > 55000):
-		wages = wages * 0.35 - 5505
-	elif(wages > 35000):
-		wages = wages * 0.3 - 2755
-	elif(wages > 9000):
-		wages = wages * 0.25 - 1005
-	elif(wages > 4500):
-		wages = wages * 0.2 - 555
-	elif(wages > 1500):
-		wages = wages * 0.1 - 105
-	elif(wages > 0 ):
-		wages = wages * 0.03
-	else:
-		wages = 0
-	return income - wages - income * 0.165
-
-
-try:
-	if(len(sys.argv[1:])>=1):
-		for arg in sys.argv[1:]:
-			Str = arg.split(':')
-			if(not (isinstance(int(Str[0]),int) and isinstance(int(Str[1]),int))):
-				raise
-		for arg in sys.argv[1:]:
-			Str = arg.split(':')
-			worker_ID = int(Str[0])
-			Para = int(Str[1])
-			result = calculate(Para)
-			print("{}:{:.2f}".format(worker_ID,result))
-	else:
-		raise
-except:
-	print("Parameter Error")
