38fb0fe5ffa13538561f5122b52ca7541edc98e7
ljt1469 <676574039@qq.com>
2017-11-29 14:07:31 +0800

    Update app.py

--- news/app.py
+++ news/app.py
@@ -27,25 +27,24 @@ class File(db.Model):
         self.content = content
         
 class Category(db.Model):
-                                                                                __tablename__ = 'categories'
-                                                                                id = db.Column(db.Integer, primary_key=True)
-                                                                                name = db.Column(db.String(80))
-                                                                                files = db.relationship('File')
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    files = db.relationship('File')
 
-                                                                                def __init__(self, name):
-                                                                                    self.name = name
+	def __init__(self, name):
+    	self.name = name
 
-
-                                                                            @app.route('/')
-                                                                            def index():
-                                                                                return render_template('index.html', files=File.query.all())
-                                                                            @app.route('/files/<int:file_id>')
-                                                                            def file(file_id):
-                                                                                file_item = File.query.get_or_404(file_id)
-                                                                                return render_template('file.html', file_item=file_item)
+@app.route('/')
+def index():
+	return render_template('index.html', files=File.query.all())
+@app.route('/files/<int:file_id>')
+def file(file_id):
+	file_item = File.query.get_or_404(file_id)
+	return render_template('file.html', file_item=file_item)
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
-                                                                            if __name__ == '__main__':
-                                                                                app.run()
+if __name__ == '__main__':
+	app.run()
